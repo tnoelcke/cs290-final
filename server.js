1e4c75fb5225244423bd0e366cc0f4a8d0fe5136
tnoelcke <noelcket@oregonstate.edu>
2016-11-30 12:26:25 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,34 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var mysql = require('mysql');
 
 // sets up the app and tells the server what port to listen on
 var app = express();
 var port = process.env.port || 3000;
+
+//setting up server to connect to sql data base
+var mysqlHost = "mysql.cs.orst.edu";
+var mysqlUser = "cs290_noelcket";
+var mysqlPassword = "9959";
+var mysqlDB = "cs290_noelcket";
+var mysqlConnection = mysql.createConnection({
+    host: mysqlHost,
+    user: mysqlUser,
+    password: mysqlPassword,
+    database: mysqlDB
+});
+
+//make a connection to our data base, connection will persist for as long as
+// our server is running
+mysqlConnection.connect(function (err) {
+    if (err) {
+        console.log("== Unable to make connection to MySQL Database.");
+        throw err;
+    } else {
+        console.log("== Connected to: ", mysqlHost, " using user: ", mysqlUser);
+    }
+});
 
 //sets up the view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -46,17 +70,49 @@ app.get('/high-scores', function (req, res) {
 app.get('/user-scores', function (req, res) {
     res.render('userscore-page', {
         pageTitle: 'test',
-        user: users,
-        userName: 'Poop'
+        user: users
     });
 });
 
 //renders the pong page
 app.get('/pong', function (req, res) {
     res.render('pong-page', {
-        pageTitle: 'PongZone',
+        pageTitle: 'Welcome to the Pong Zone',
         userName: 'Test'
     });
+});
+
+app.post('/score/:userid', function (req, res, next) {
+    if (req.body) {
+        mysqlConnection.query(
+            'INSERT INTO Scores(userName, Score) VALUES (?, ?)',
+            [userid, req.body.score], 
+                function (err, result) {
+                    if (err) {
+                        console.log("==Error Adding score into Data Base: " + err)
+                        res.status(500).send("error adding score into DB: " + error)
+                    }
+                    res.status(200).send();
+                });
+    } else {
+        res.status(400).send("no score sent!");
+    }
+});
+
+app.post('/newuser', function (req, res, next) {
+    if (req.body) {
+        mysqlConnection.query(
+        'INSERT INTO PongGame (userName, Password) VALUES (?,?)', [req.body.user.userId, req.body.user.password],
+        function (err, result) {
+            if (err) {
+                console.log("==Error Adding User into Data Base: ", err);
+                res.status(500).send("USER NAME already taken please enter a different name");
+            }
+            res.status(200).send();
+        });
+    } else {
+        res.status(400).send("Incompleate data");
+    }
 });
 
 //render the 404 page for any page that doesn't exists
